bc12e49f7672491ed387e2781530a04f570be394
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-04 17:55:20 +0300

    :construction: implement redirect

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Sidebar.scss';
+import { useNavigate } from 'react-router-dom';
 import * as images from '../../assets';
 import * as icons from '../../assets/icons';
 import { Button } from '../Button';
 import { NavigationMenu } from '../NavigationMenu';
+import { RoutePath } from '../../routes';
 
 export const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleRedirectClick = () => {
+    navigate(RoutePath.users);
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar__user-info user-info">
@@ -19,6 +27,7 @@ export const Sidebar = () => {
           buttonStyles="user-info__button button"
           iconStyles="button__icon"
           icon={icons.gear}
+          onClick={handleRedirectClick}
         />
       </div>
 
